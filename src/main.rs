6b99811c740cac0d59bab481d6a49c6a3b820f45
Tamir Bahar <tamir.bahar@gmail.com>
2023-11-05 14:21:14 +0200

    Added scaling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use image::{io::Reader as ImageReader, DynamicImage, GenericImageView};
+use image::{
+    imageops::FilterType, io::Reader as ImageReader, DynamicImage, GenericImageView, RgbImage,
+};
 use rust_xlsxwriter::{Color, Format, Workbook, XlsxError};
 
-fn worksheet_from_image(img: &DynamicImage) -> Result<Workbook, XlsxError> {
+fn worksheet_from_image(img: &RgbImage) -> Result<Workbook, XlsxError> {
     let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet();
 
@@ -9,8 +11,7 @@ fn worksheet_from_image(img: &DynamicImage) -> Result<Workbook, XlsxError> {
         worksheet.set_column_width(x as u16, 2)?;
         for y in 0..img.height() {
             let pixel = img.get_pixel(x, y);
-            let color =
-                ((pixel[0] as u32) << 16) + ((pixel[1] as u32) << 8) + (pixel[2] as u32);
+            let color = ((pixel[0] as u32) << 16) + ((pixel[1] as u32) << 8) + (pixel[2] as u32);
             let format = Format::new().set_background_color(Color::RGB(color));
             worksheet.write_string_with_format(y, x as u16, "", &format)?;
         }
@@ -25,7 +26,11 @@ fn main() -> Result<(), XlsxError> {
         Err(err) => panic!("{}", err),
     };
 
-    let mut workbook = worksheet_from_image(&img)?;
-    workbook.save("image.xlsx")?;
+    if let Some(img) = img.as_rgb8() {
+        let img = image::imageops::resize(img, 50, 50, FilterType::Gaussian);
+
+        let mut workbook = worksheet_from_image(&img)?;
+        workbook.save("image.xlsx")?;
+    }
     Ok(())
 }
